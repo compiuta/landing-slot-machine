c61cbca1e72595a1159beff55ed2f6c160c703e3
compiuta <christianr@rocketmail.com>
2020-07-23 20:02:49 -0700

    current app build

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,8 +1,29 @@
 (function (window) {
     'use-strict';
-    const test ='test';
 
-    console.log(test);
+    const slotLandingContainer = document.querySelector('[data-slot-landing]');
+    const mobileMenu = document.querySelector('[data-mobile-menu]');
+    const navElement = document.querySelector('[data-main-nav]');
+    const buttonScrollUp = document.querySelector('[data-scroll-up]');
+    const ButtonScrollDown = document.querySelector('[data-scroll-down]');
+    let menuItemSize;
+    let navElementSize;
+    let currentMenuItem = 0;
 
-    
+    function toggleMobileMenu() {
+        slotLandingContainer.classList.toggle('mobile-menu-open');
+    }
+
+    function calculateElementsSize() {
+        console.log('resized');
+    }
+
+    function navScroll(e) {
+        
+    }
+
+    mobileMenu.addEventListener('click', toggleMobileMenu);
+    buttonScrollUp.addEventListener('click', navScroll);
+    ButtonScrollDown.addEventListener('onmousedown', navScroll);
+    window.addEventListener('resize', calculateElementsSize)
 })(window);
