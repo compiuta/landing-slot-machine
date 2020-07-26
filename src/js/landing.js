bb4aaf293b6c8d484a866187609730f7af7476cd
compiuta <christianr@rocketmail.com>
2020-07-26 10:52:47 -0700

    add modal functionality and animation

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,12 +1,15 @@
 (function (window) {
     'use-strict';
 
+    const bodyTag = document.querySelector('[data-body-tag]');
     const slotLandingContainer = document.querySelector('[data-slot-landing]');
     const mobileMenu = document.querySelector('[data-mobile-menu]');
     const navElement = document.querySelector('[data-main-nav]');
     const buttonScrollUp = document.querySelector('[data-scroll-up]');
     const buttonScrollDown = document.querySelector('[data-scroll-down]');
     const navModal = document.querySelector('[data-nav-modal]');
+    const navWrap = document.querySelector('[data-nav-wrapper]');
+    const modalClose = document.querySelector('[data-modal-close]');
     const slotItems = [
         'diamond-active',
         'seven-active',
@@ -48,15 +51,20 @@
         }
     }
 
+    function toggleTopTransition() {
+        navElement.classList.toggle('nav-top-transition');
+    }
+
     function toggleCurrentMenuItem() {
-        navElement.classList.toggle(slotItems[currentMenuItem]);
-        navModal.classList.toggle(slotItems[currentMenuItem]);
+        navWrap.classList.toggle(slotItems[currentMenuItem]);
     }
 
     function navScroll(e) {
         const clickedButton = e.currentTarget;
 
         toggleCurrentMenuItem();
+
+        toggleTopTransition();
 
         if (clickedButton.classList.contains('scroll-button-down')) {
             currentMenuItem += 1;
@@ -67,11 +75,36 @@
         toggleCurrentMenuItem();
         toggleSliderButtons();
         navElement.style.top = `-${currentMenuItem * menuItemSize}px`;
+
+        setTimeout(() => {
+            toggleTopTransition();
+        }, 200);
+    }
+
+    function openModal() {
+        navModal.classList.remove('hide');
+        bodyTag.classList.add('animation-activated');
+
+        setTimeout(() => {
+            
+        }, 1000);
+    }
+
+    function closeModal() {
+        bodyTag.classList.remove('animation-activated');
+        bodyTag.classList.add('animate-close');
+
+        setTimeout(() => {
+            navModal.classList.add('hide');
+            bodyTag.classList.remove('animate-close');
+        }, 1000);
     }
 
     mobileMenu.addEventListener('click', toggleMobileMenu);
     buttonScrollUp.addEventListener('click', navScroll);
     buttonScrollDown.addEventListener('click', navScroll);
+    navElement.addEventListener('click', openModal);
+    modalClose.addEventListener('click', closeModal);
     window.addEventListener('resize', resizeElements);
 
     calculateElementsSize();
